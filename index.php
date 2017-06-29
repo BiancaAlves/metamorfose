89adb9dfacfc9961cd595fc067619b2599baab48
Bianca <bianca.alves2011@hotmail.com>
2017-06-29 13:17:41 -0300

    Primeiro commit

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<head>
+	<meta name="viewport" content="width-device-width, initial-scale=1.0">
+	<meta charset="UTF-8">
+	<title>Metamorfose Digital :: Trabalho na Internet e negócios online</title>
+	<link rel="stylesheet" href="node_modules/bootstrap/dist/css/bootstrap.css">
+	<link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+	<!-- HEADER STARTS -->
+	
+
+	<script src="node_modules/bootstrap/dist/js/bootstrap.mn.js"></script>
+</body>
+</html>
