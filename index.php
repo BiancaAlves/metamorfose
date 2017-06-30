edcd619e7a871aa119e15d27cdb27acc9a6189e7
Bianca <bianca.alves2011@hotmail.com>
2017-06-29 23:47:57 -0300

    Front da página index construída

--- index.php
+++ index.php
@@ -1,15 +1,302 @@
 <!DOCTYPE html>
+<html>
 <head>
-	<meta name="viewport" content="width-device-width, initial-scale=1.0">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<meta charset="UTF-8">
 	<title>Metamorfose Digital :: Trabalho na Internet e negócios online</title>
-	<link rel="stylesheet" href="node_modules/bootstrap/dist/css/bootstrap.css">
+	<link rel="stylesheet" href="node_modules/bootstrap-custom/css/bootstrap.css" media="all"> 
+	<link rel="stylesheet" href="node_modules/font-awesome/css/font-awesome.min.css" media="all"> 
 	<link rel="stylesheet" href="css/style.css">
+	<!-- Fonts -->
+	<link href="https://fonts.googleapis.com/css?family=Lato:300,400,700" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Raleway:300,300i,400,800" rel="stylesheet">
 </head>
 <body>
 	<!-- HEADER STARTS -->
-	
+	<nav class="navbar navbar-default" role="navigation">
+		<div class="container-fluid">
+			<div class="col-md-10 col-md-offset-1 col-lg-10 col-lg-offset-1">
+					<button type="button" class="navbar-toggle collapsed pull-left" data-toggle="collapse" data-target="#elementoCollapse1" aria-expanded="false" aria-controls="navbar">
+						<span class="sr-only">Toggle navigation</span>
+				        <span class="icon-bar"></span>
+				        <span class="icon-bar"></span>
+				        <span class="icon-bar"></span>
+			      	</button>
+		      	<div class="collapse navbar-collapse" id="elementoCollapse1">
+					<ul class="nav navbar-nav">
+						<li class="active"><a href="#">Início</a></li>
+						<li class="dropdown">
+							<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Categorias <span class="caret"></span></a>
+							<ul class="dropdown-menu">
+								<li><a href="#">Divulgação no Facebook</a></li>
+								<li><a href="#">Mercado Livre</a></li>
+								<li><a href="#">Afiliado</a></li>
+							</ul>
+						</li>
+						<li><a href="#">Sobre</a></li>
+						<li><a href="#">Contato</a></li>
+					</ul>
 
-	<script src="node_modules/bootstrap/dist/js/bootstrap.mn.js"></script>
+					<div class="navbar-nav btn-toolbar navbar-right .social-group">
+						<a href="#">
+						<button type="button" class="btn btn-default navbar-btn navbar-right social-button gplus"><i class="fa fa-google-plus"></i></button>
+						</a>
+						<a href="#">
+						<button type="button" class="btn btn-default navbar-btn navbar-right social-button instagram"><i class="fa fa-instagram"></i></button>
+						</a>
+						<a href="#">
+						<button type="button" class="btn btn-default navbar-btn navbar-right social-button facebook"><i class="fa fa-facebook"></i>
+						</button>
+						</a>
+						
+						
+					</div>
+
+					<form class="navbar-form navbar-right form-inline search" role="search">
+						<div class="form-group">
+							<input type="text" placeholder="Pesquisar" class="form-control search-input">
+						</button>
+						</div>
+						<button type="submit" value="Enviar" class="btn btn-default search-button">
+							<span class="glyphicon glyphicon-search"></span>
+						</button>
+					</form>
+				</div>
+			</div>
+		</div>
+	</nav>
+
+	<!-- Logo -->
+	<div class="container-fluid">
+		<div class="row logo">
+			<div class="col-xs-8 col-xs-offset-2 col-sm-5 col-sm-offset-0 col-md-4 col-md-offset-1 col-lg-4 col-lg-offset-1">
+				<img src="img/logo.png" class="img-responsive center-block  mdb-logo" alt="Metamorfose Digital">
+			</div>
+		</div>
+	</div>
+
+	<!-- HEADER ENDS -->
+	<!-- HOME STARTS -->
+	<!-- Carousel -->
+
+	<div class="carousel-container">
+		<div class="row mdb-carousel">
+			<div id="carousel01" class="carousel slide" data-ride="carousel">
+
+			<div class="carousel-inner">
+				<div class="item active carousel-img">
+					<img src="img/slider_1.jpg" alt="Foto 1">
+					<div class="carousel-caption">
+						<span class="label label-danger">Hashtag 1</span>
+						<span class="label label-warning">Hashtag 2</span>
+						<h1>Nulla feugiat vitae elit eget rhoncus</h1>		
+						<p>Praesent euismod enim nibh, at egestas nulla rutrum eget</p>
+					</div>
+				</div>
+				<div class="item carousel-img">
+					<img src="img/slider_2.jpg" alt="Foto 2">
+					<div class="carousel-caption">
+						<span class="label label-danger">Hashtag 1</span>
+						<span class="label label-warning">Hashtag 2</span>
+						<h1>Donec blandit interdum bibendum</h1>		
+						<p>Vivamus at risus accumsan, pharetra mauris ut, convallis nibh</p>
+					</div>
+				</div>
+				<div class="item carousel-img">
+					<img src="img/slider_3.jpg" alt="Foto 3">
+					<div class="carousel-caption">
+						<span class="label label-danger">Hashtag 1</span>
+						<span class="label label-warning">Hashtag 2</span>
+						<h1>Duis feugiat, libero non fermentum interdum, mauris mauris sodales turpis</h1>		
+						<p>Vestibulum finibus orci vitae odio aliquam condimentum</p>
+					</div>
+				</div>
+				<div class="item carousel-img">
+					<img src="img/slider_4.jpg" alt="Foto 4">
+					<div class="carousel-caption">
+						<span class="label label-danger">Hashtag 1</span>
+						<span class="label label-warning">Hashtag 2</span>
+						<h1>Mauris vestibulum dolor sit amet commodo dignissim</h1>		
+						<p>Sed at ex ut risus consequat posuere a eu enim</p>
+					</div>
+				</div>
+			</div>
+
+			<a class="left carousel-control" href="#carousel01" role="button" data-slide="prev">
+				<span class="glyphicon glyphicon-chevron-left"></span>
+			</a>
+
+			<a class="right carousel-control" href="#carousel01" role="button" data-slide="next">
+				<span class="glyphicon glyphicon-chevron-right"></span>
+			</a>
+
+			</div>
+		</div>
+	</div>
+
+	<!-- Posts -->
+
+	<div class="container">
+		<div class="row">
+			<div class="col-xs-12 col-md-7">
+				<div class="row home-post">
+					<div class="post-img">
+						<img src="img/slider_1.jpg" class="img-responsive">
+					</div>
+					<div class="post-text">
+						<h1>Lorem ipsum dolor sit amet, </h1>
+						<h2>Sed vehicula mollis fringilla. Donec ac rutrum nisl. Suspendisse pretium euismod imperdiet.</h2>
+						<button type="button" class="btn btn-default pull-right body-btn">Continue lendo <span class="glyphicon glyphicon-arrow-right"></span></button>
+					</div>
+				</div>
+				
+				<div class="row home-post">
+					<div class="post-img">
+						<img src="img/slider_2.jpg" class="img-responsive">
+					</div>
+					<div class="post-text">
+						<h1>Lorem ipsum dolor sit amet, </h1>
+						<h2>Sed vehicula mollis fringilla. Donec ac rutrum nisl. Suspendisse pretium euismod imperdiet.</h2>
+						<button type="button" class="btn btn-default pull-right body-btn">Continue lendo <span class="glyphicon glyphicon-arrow-right"></span></button>
+					</div>
+				</div>
+				
+				<div class="row home-post">
+					<div class="post-img">
+						<img src="img/slider_3.jpg" class="img-responsive">
+					</div>
+					<div class="post-text">
+						<h1>Lorem ipsum dolor sit amet, </h1>
+						<h2>Sed vehicula mollis fringilla. Donec ac rutrum nisl. Suspendisse pretium euismod imperdiet.</h2>
+						<button type="button" class="btn btn-default pull-right body-btn">Continue lendo <span class="glyphicon glyphicon-arrow-right"></span></button>
+					</div>
+				</div>
+
+				<!-- Paginação -->
+				<div class="row">
+					<div class="pagination center-block text-center">
+						<button type="button" class="btn btn-default">1</button>
+						<button type="button" class="btn btn-default">2</button>
+						<button type="button" class="btn btn-default">3</button>
+						<button type="button" class="btn btn-default">4</button>
+						<button type="button" class="btn btn-default">5</button>
+						<button type="button" class="btn btn-default body-btn">Next <span class="fa-angle-double-right"></span></button>
+					</div>
+				</div>
+			</div>
+
+			<!-- HOME END -->
+			<!-- SIDEBAR START: A sidebar ocupa 4 colunas médias -->
+
+			<div class="col-xs-12 col-md-3 col-md-offset-1">
+				<div class="row sidebar-block">
+					<h3>Mais lidos</h3>
+
+					<ul class="media-list">
+						<li class="media mini-post">
+							<a href="#" class="mini-post-img">
+								<img src="img/slider_1.jpg">
+							</a>							
+									<a href="#">
+										<h4 class="media-heading">Ut aliquam, metus et tristique vehicula, erat felis dapibus est</h4>
+										<p>Sed at ex ut risus consequat posuere a eu enim.</p>
+									</a>							
+						</li>
+
+						<li class="media mini-post">
+							<a href="#" class="mini-post-img">
+								<img src="img/slider_2.jpg">
+							</a>
+								<a href="#">
+									<h4 class="media-heading">Nulla quis lacus volutpat</h4>
+									<p>Donec blandit interdum bibendum.</p>
+								</a>							
+						</li>
+
+						<li class="media mini-post">
+							<a href="#" class="mini-post-img">
+								<img src="img/slider_3.jpg">
+							</a>
+								<a href="#">
+									<h4 class="media-heading">Nulla at nulla massa.</h4>
+									<p>Nulla non tellus ante.
+
+</p>
+								</a>
+						</li>
+					</ul>
+				</div>
+
+				<div class="row sidebar-block">
+					<h3>Sobre a autora</h3>
+						<div class="media">
+							<div class="pull-left profile">
+								<img src="img/slider_4.jpg" >
+							</div>
+							<div class="media-body">
+								<p>Texto texto texto texto texto texto texto texto texto texto texto texto texto texto texto</p>
+
+								<button type="button" class="btn btn-defaut pull-right body-btn">Continue lendo <span class="glyphicon glyphicon-arrow-right"></span></button>
+							</div>
+						</div>
+				</div>
+
+				<div class="row sidebar-block">
+					<h3>Fique atualizado</h3>
+					<p>Assine nossa newsletter e fique por dentro de tudo sobre trabalho na Internet!</p>
+					<form>
+						<div class="form-group">
+							<input type="text" name="nome" class="form-control" placeholder="Nome" />
+						</div>
+						<div class="form-group">
+							<input type="email" name="email" class="form-control" placeholder="E-mail" />
+						</div>
+							<button type="button" class="btn btn-default btn-block body-btn">Enviar</button>
+					</form>
+				</div>
+
+			</div>
+		</div>
+	</div>
+
+	<!-- SIDEBAR END -->
+	<!-- FOOTER START -->
+	<footer class="footer">
+		<div class="well">
+			<div class="container">
+				<div class="col-xs-12 col-md-8">
+					<div class=".footer-content">
+						<p>Metamorfose Digital Blog © Todos os direitos reservados</p>
+						<small>Desenvolvido por <a href="#">Bianca</a></small>
+					</div>
+				</div>
+				<div class="col-xs-12 col-md-4">
+					<div class="pull-right footer-content">
+						<a href="#">
+						<button type="button" class="btn btn-default navbar-btn navbar-right social-button gplus"><i class="fa fa-google-plus"></i></button>
+						</a>
+						<a href="#">
+						<button type="button" class="btn btn-default navbar-btn navbar-right social-button instagram"><i class="fa fa-instagram"></i></button>
+						</a>
+						<a href="#">
+						<button type="button" class="btn btn-default navbar-btn navbar-right social-button facebook"><i class="fa fa-facebook"></i>
+						</button>
+						</a>
+					</div>
+				</div>
+				</div>
+			</div>
+		</div>
+	</footer>
+
+
+	<script src="node_modules/jquery/dist/jquery.min.js"></script>
+	<script src="node_modules/bootstrap/dist/js/bootstrap.min.js"></script>
+
+	<!--
+	<script src="node_modules/jquery/dist/jquery.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
+	-->
 </body>
 </html>
---
+++ readme.md
@@ -0,0 +1,3 @@
+# Metamorfose
+
+Tema Wordpress em construção para o blog **Metamorfose Digital.**
